770e5076ee12b9379bb04c5688ba8af391329e69
ndaemy <yuyaebean@gmail.com>
2023-01-15 18:37:00 +0900

    🔧 Add member-ordering lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,18 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/member-ordering': [
+      'error',
+      {
+        default: [
+          'signature',
+          'field',
+          'constructor',
+          'private-method',
+          'protected-method',
+          'public-method',
+        ],
+      },
+    ],
   },
 };
