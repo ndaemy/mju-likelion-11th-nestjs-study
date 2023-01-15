88b06532619e58b6b73132f7c37b1f3bc1424ba7
ndaemy <yuyaebean@gmail.com>
2023-01-15 13:52:21 +0900

    ✨ 3장 완료

---
+++ src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserInfo } from './user-info';
+
+@Controller('users')
+export class UsersController {
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<void> {
+    console.log(dto);
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return '';
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return {
+      id: userId,
+      name: 'John Doe',
+      email: '',
+    };
+  }
+}
