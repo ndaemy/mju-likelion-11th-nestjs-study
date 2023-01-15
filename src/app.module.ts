ae90a59a689cf18a85842f97525d2242b60283af
ndaemy <yuyaebean@gmail.com>
2023-01-15 17:09:28 +0900

    ✨ 4장 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { EmailService } from './email/email.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService, EmailService],
 })
 export class AppModule {}
---
+++ src/email/email.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { createTransport } from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+
+@Injectable()
+export class EmailService {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = this.createTransporter();
+  }
+
+  private createTransporter(): Mail {
+    return createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  }
+
+  async sendMemberJoinVerification(email: string, signupVerifyToken: string) {
+    const baseUrl = process.env.BASE_URL;
+
+    const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
+
+    const mailOptions = {
+      to: email,
+      subject: '가입 인증 메일',
+      html: `
+        가입확인 버튼을 누르시면 가입 인증이 완료됩니다.<br />
+        <form action="${url}" method="POST">
+          <button type="submit">가입확인</button>
+        </form>
+      `,
+    };
+
+    return await this.transporter.sendMail(mailOptions);
+  }
+}
