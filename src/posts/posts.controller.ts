e00fe18e50f10864f662cfa177698234594dee61
ndaemy <yuyaebean@gmail.com>
2023-01-18 03:00:42 +0900

    ✨ Implement read related controllers

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postsService.findOne(id);
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { validate as uuidValidate } from 'uuid';
+
+export type Post = {
+  id: string;
+  caption: string;
+  // TODO: 이 부분은 추후 user 모델과 연결해야 함
+  writer: {
+    email: string;
+  };
+};
+
+const INITIAL_POSTS: Post[] = [
+  {
+    id: '84ac5723-5170-4dc1-b49a-c40ee8264d36',
+    caption: 'Hello World!',
+    writer: {
+      email: 'yuyaebean@gmail.com',
+    },
+  },
+  {
+    id: 'd0eb7cf3-f258-49f9-8497-efd93daa5f84',
+    caption: 'KT 왜 씀',
+    writer: {
+      email: 'harksulim@gmail.com',
+    },
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  private posts: Post[] = INITIAL_POSTS;
+
+  findAll() {
+    return this.posts;
+  }
+
+  findOne(id: string) {
+    if (!uuidValidate(id)) {
+      throw new BadRequestException('id is not valid form');
+    }
+
+    const post = this.posts.find((post) => post.id === id);
+
+    if (!post) {
+      throw new NotFoundException(`Post with id ${id} not found`);
+    }
+
+    return post;
+  }
+}
