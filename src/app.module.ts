bf1e48f848ba4f5b2ab8051dddc700d19e5b2cdd
ndaemy <yuyaebean@gmail.com>
2023-01-18 02:32:50 +0900

    🚧 Create posts controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { EmailService } from './email/email.service';
+import { PostsController } from './posts/posts.controller';
+import { PostsService } from './posts/posts.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService, EmailService],
+  controllers: [AppController, UsersController, PostsController],
+  providers: [AppService, UsersService, EmailService, PostsService],
 })
 export class AppModule {}
