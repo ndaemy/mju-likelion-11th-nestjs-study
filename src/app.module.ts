e98024219fdd801b4520d52aedab16ff87b951c9
ndaemy <yuyaebean@gmail.com>
2023-01-19 02:28:32 +0900

    ♻️ Modularize posts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,11 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { EmailService } from './email/email.service';
-import { PostsController } from './posts/posts.controller';
-import { PostsService } from './posts/posts.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController, UsersController, PostsController],
-  providers: [AppService, UsersService, EmailService, PostsService],
+  imports: [ConfigModule.forRoot(), PostsModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService, EmailService],
 })
 export class AppModule {}
