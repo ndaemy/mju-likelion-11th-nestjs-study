382016a95a0e195614036bdb85de28a115e4d90d
ndaemy <yuyaebean@gmail.com>
2023-02-05 23:14:55 +0900

    ✨ 7장 완료

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,25 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   readonly name: string;
+
+  @IsString()
+  @IsEmail()
+  @MaxLength(60)
   readonly email: string;
+
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  @Matches(/^[A-Za-z0-9!@#$%^&*()]{8,30}$/)
   readonly password: string;
 }
